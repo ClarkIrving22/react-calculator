901f0d3b48ce5a57326a213ac1f87d68a59eba6c
ClarkIrving22 <83539806+ClarkIrving22@users.noreply.github.com>
2023-04-26 11:20:25 -0500

    Add files via upload

---
+++ src/components/botonclear/BotonClear.jsx
@@ -0,0 +1,15 @@
+import './styles.css'
+
+const BotonClear = (
+  props
+) => {
+  return(
+    <div
+      className='boton-clear'
+      onClick={props.manejarClear}>
+      {props.children}
+    </div>
+  )
+}
+
+export default BotonClear
---
+++ src/components/pantalla/Pantalla.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+const Pantalla = ({
+  input
+}) => {
+  return(
+    <div className='input'>
+      {input}
+    </div>
+  )
+}
+
+export default Pantalla
